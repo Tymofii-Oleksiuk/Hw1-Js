b873a646c9293e48c2937d8500094f054356745a
Tymofii-Oleksiuk <jji243372@gmail.com>
2024-08-30 21:51:05 +0200

    HomeWork

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let MyAge = 14;
+console.log(MyAge);
+const MyName = "Тимофій";
+console.log(MyName);
+let isStudent = "true";
+console.log(isStudent);
+const myString = "Добив а там пофіг";
+console.log(myString);
+const myNumber = 1478 + 10;
+console.log(myNumber);
+const myNull = (null);
+console.log(myNull)
