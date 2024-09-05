59322de7c79b9bf53a88f800fdb0a8e9e7aa4a71
Tymofii-Oleksiuk <jji243372@gmail.com>
2024-09-05 22:03:51 +0200

    Hw1-JS

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
+
+
 let MyAge = 14;
 console.log(MyAge);
+
 const MyName = "Тимофій";
 console.log(MyName);
+
 let isStudent = "true";
 console.log(isStudent);
+
 const myString = "Добив а там пофіг";
 console.log(myString);
+
 const myNumber = 1478 + 10;
 console.log(myNumber);
+
 const myNull = (null);
 console.log(myNull)
+
+const message = "Ведіть своє ім'я";
+prompt(message);
+
+const confirm1 = ("Підтвердіть своє імя");
+confirm(confirm1);
+
+const dangerous = ("Ця дія може зашкодити вашому комп'ютеру.");
+alert (dangerous);
